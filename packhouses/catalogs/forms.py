998199abc98da8caf2e702aa555e6a0ceb56405b
Niji <mathereall@gmail.com>
2024-10-29 07:21:48 -0600

    ProductVarietySize

--- packhouses/catalogs/forms.py
+++ packhouses/catalogs/forms.py
@@ -39,30 +39,34 @@ class ProductVarietySizeInlineForm(forms.ModelForm):
         queryset=MarketStandardProductSize.objects.none(),
         required=False,
         label=_('Market Standard Product Sizes'),
-        help_text=_('Choose a Standard Product Size per Market (optional), it will put its name in the size name field.'),
+        help_text=_(
+            'Choose a Standard Product Size per Market (optional), it will put its name in the size name field.'),
         widget=forms.Select(attrs={'readonly': 'readonly'})
     )
+
+    class Meta:
+        model = ProductVarietySize
+        fields = ['market', 'market_standard_product_size', 'name', 'alias', 'quality_kind', 'harvest_kind',
+                  'description', 'product_kind', 'requires_corner_protector', 'is_enabled', 'order']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if 'market' in self.data:
             try:
                 market_id = int(self.data.get('market'))
-                self.fields['market_standard_product_size'].queryset = MarketStandardProductSize.objects.filter(market_id=market_id)
+                self.fields['market_standard_product_size'].queryset = MarketStandardProductSize.objects.filter(
+                    market_id=market_id)
+                if MarketStandardProductSize.objects.filter(name=self.name, market_id=market_id).exists():
+                    self.fields['market_standard_product_size'].initial = MarketStandardProductSize.objects.get(
+                        name=self.name, market_id=market_id)
             except (ValueError, TypeError) as e:
-                pass
+                print(e)
         elif self.instance.pk and self.instance.market:
-            self.fields['market_standard_product_size'].queryset = MarketStandardProductSize.objects.filter(market=self.instance.market)
+            self.fields['market_standard_product_size'].queryset = MarketStandardProductSize.objects.filter(
+                market=self.instance.market)
+            if MarketStandardProductSize.objects.filter(name=self.instance.name, market=self.instance.market).exists():
+                self.fields['market_standard_product_size'].initial = MarketStandardProductSize.objects.get(
+                    name=self.instance.name, market=self.instance.market)
 
-    """
     class Media:
-        js = ('js/admin/forms/product_variety_inline_size.js',)
-    """
-
-    class Meta:
-        model = ProductVarietySize
-        fields = ['market', 'market_standard_product_size', 'name', 'alias', 'quality_kind', 'harvest_kind',
-                  'description', 'product_kind', 'requires_corner_protector', 'is_enabled', 'order']
-        widgets = {
-            # 'product_kind': RadioSelect(),  # Usa RadioSelect para el campo ForeignKey
-        }
+        js = ('js/admin/forms/product_variety_size.js',)
