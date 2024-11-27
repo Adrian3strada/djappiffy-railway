026aa3b9256f39e7035bb2c3ce11391fae2ae25f
Ismael Olvera <18491854+iOlvera56@users.noreply.github.com>
2024-11-27 14:28:35 -0600

    feat(model-admins): Substitute 'OrganizationUserAdmin' register
    Substitute the orginal 'OrganizationUserAdmin' register implemented
    by 'django-organizations' for 'CustomOrganizationUserAdmin'
    which extends the original one.

--- common/base/admin.py
+++ common/base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from organizations.admin import OrganizationAdmin
-from organizations.models import Organization
+from organizations.admin import OrganizationAdmin, OrganizationUserAdmin
+from organizations.models import Organization, OrganizationUser
 
 from .models import ProductKind
 
@@ -15,5 +15,15 @@ class CustomOrganizationAdmin(OrganizationAdmin):
     list_display = ["id", "name", "slug", "created", "modified", "is_active"]
     ordering = ["id"]
 
+
+class CustomOrganizationUserAdmin(OrganizationUserAdmin):
+    list_display = ["id", "created", "modified", "is_admin",
+                    "organization", "user"]
+    list_display_links = ["id", "organization", "user"]
+    ordering = ["id"]
+
+
 admin.site.unregister(Organization)
 admin.site.register(Organization, CustomOrganizationAdmin)
+admin.site.unregister(OrganizationUser)
+admin.site.register(OrganizationUser, CustomOrganizationUserAdmin)
