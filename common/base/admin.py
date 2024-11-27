26907c2b2f174b3faad8f413da9fda97d177b6ec
Ismael Olvera <18491854+iOlvera56@users.noreply.github.com>
2024-11-27 13:24:36 -0600

    feat(model-admins): Substitute 'OrganizationAdmin' register
    Substitute the orginal 'OrganizationAdmin' register implemented by
    'django-organizations' for 'CustomOrganizationAdmin' which extends
    the original one.
    
    Organize the import sentences by type and alphabetically.
    
    Delete the automatically generated initial comment.

--- common/base/admin.py
+++ common/base/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import ProductKind
 
-# Register your models here.
+from organizations.admin import OrganizationAdmin
+from organizations.models import Organization
+
+from .models import ProductKind
 
 
 @admin.register(ProductKind)
 class ProductKindAdmin(admin.ModelAdmin):
     pass
+
+
+class CustomOrganizationAdmin(OrganizationAdmin):
+    list_display = ["id", "name", "slug", "created", "modified", "is_active"]
+    ordering = ["id"]
+
+admin.site.unregister(Organization)
+admin.site.register(Organization, CustomOrganizationAdmin)
