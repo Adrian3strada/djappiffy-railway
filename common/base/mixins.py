f7640745f96531154bfff7e1b1e2041e64ea7223
Cesar Benjamin <mathereall@gmail.com>
2024-12-30 23:47:22 -0600

    feat(Product) adición de filtro para ProductKind

--- common/base/mixins.py
+++ common/base/mixins.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 
 
-class OrganizationAdminMixin(admin.ModelAdmin):
+class ByOrganizationAdminMixin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if hasattr(request, 'organization'):
@@ -14,3 +14,12 @@ class OrganizationAdminMixin(admin.ModelAdmin):
             if hasattr(request, 'organization'):
                 obj.organization = request.organization
         super().save_model(request, obj, form, change)
+
+
+# TODO: creo que esta puede estar mal... revisar
+class ByProductAdminMixin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if hasattr(request, 'product'):
+            return qs.filter(product=request.product, is_enabled=True)
+        return qs
