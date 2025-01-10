89f8417da15a2d3f30f5440bc801ca2e88b9f8c7
Ismael Olvera <18491854+iOlvera56@users.noreply.github.com>
2025-01-10 12:40:05 -0600

    feat(packaging): Agrega js usado por 'ProductPackaging'

---
+++ application/static/js/admin/forms/packhouses/catalogs/product_packaging.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const marketField = $('#id_market');
+  const marketClassField = $('#id_market_class');
+  const productField = $('#id_product');
+  const productVarietyField = $('#id_product_variety');
+
+  const API_BASE_URL = '/rest/v1';
+
+  function updateFieldOptions(field, options) {
+    field.empty().append(new Option('---------', '', true, true));
+    options.forEach(option => {
+      field.append(new Option(option.name, option.id, false, false));
+    });
+    field.trigger('change').select2();
+  }
+
+  function fetchOptions(url) {
+    return $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'json'
+    }).fail(error => console.error('Fetch error:', error));
+  }
+
+  function updateMarketClass() {
+    const marketId = marketField.val();
+    if (marketId) {
+      fetchOptions(`${API_BASE_URL}/catalogs/market_class/?market=${marketId}&is_enabled=1`)
+        .then(data => {
+          updateFieldOptions(marketClassField, data);
+        });
+    } else {
+      updateFieldOptions(marketClassField, []);
+    }
+  }
+
+  function updateProductVariety() {
+    const productId = productField.val();
+    if (productId) {
+      fetchOptions(`${API_BASE_URL}/catalogs/product_variety/?product=${productId}&is_enabled=1`)
+        .then(data => {
+          updateFieldOptions(productVarietyField, data)
+        });
+    } else {
+      updateFieldOptions(marketClassField, [])
+    }
+  }
+
+  marketField.on('change', function () {
+    updateMarketClass();
+  });
+
+  productField.on('change', function () {
+    updateProductVariety();
+  });
+
+  [marketField, marketClassField].forEach(field => field.select2());
+});
