da79a5b5a17b66c800469f523e5274be9a20386b
Edgar Gaona Zavala <gaona1526@gmail.com>
2025-02-10 20:08:07 -0600

    feat(purchase_operations/ Purchase Orders) módulo de ordenes de compra
    Falta validar filtros de organización y relalizar actualizaciones de estado propias del módulo

--- common/forms.py
+++ common/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from django.forms.models import ModelChoiceIteratorValue
 
 class SelectWidgetWithData(forms.Select):
-    def __init__(self, model, data_field, *args, **kwargs):
+    def __init__(self, model, data_fields, *args, **kwargs):
         self.model = model
-        self.data_field = data_field
+        self.data_fields = data_fields if isinstance(data_fields, list) else [data_fields]
         self._data_cache = {}
         super().__init__(*args, **kwargs)
 
@@ -18,11 +18,14 @@ class SelectWidgetWithData(forms.Select):
             if value not in self._data_cache:
                 try:
                     instance = self.model.objects.get(pk=value)
-                    data_value = getattr(instance, self.data_field)
-                    self._data_cache[value] = data_value
+                    self._data_cache[value] = {
+                        field: getattr(instance, field, '') for field in self.data_fields
+                    }
                 except self.model.DoesNotExist:
-                    self._data_cache[value] = None
+                    self._data_cache[value] = {field: '' for field in self.data_fields}
 
-            option['attrs'][f'data-{self.data_field}'] = str(self._data_cache.get(value, ''))
+            # Agregar múltiples atributos de datos
+            for field in self.data_fields:
+                option['attrs'][f'data-{field}'] = str(self._data_cache[value].get(field, ''))
 
         return option
