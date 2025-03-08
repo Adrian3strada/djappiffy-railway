4a3b7580d62d25daeaf8e77df8714305598551b0
Nannis96 <danni.mbb.96@gamil.com>
2025-03-08 12:01:23 -0600

    Tablas para la certifiscacion

--- packhouses/certifications/admin.py
+++ packhouses/certifications/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import (CertificationCatalog, CertificationDocuments, CertificationProducts, Certifications)
+
+class CertificationDocumentsInline(admin.TabularInline):
+    model = CertificationDocuments
+    extra = 1
+
+class CertificationProductsInline(admin.TabularInline):
+    model = CertificationProducts
+    extra = 1
+
+@admin.register(CertificationCatalog)
+class CertificationCatalogAdmin(admin.ModelAdmin):
+    list_display = ('certifier', 'certification', 'is_enabled')
+    list_filter = ['certifier', 'certification', 'is_enabled']
+    inlines = [CertificationDocumentsInline, CertificationProductsInline]
+
+@admin.register(Certifications)
+class CertificationsAdmin(admin.ModelAdmin):
+    list_display = ('certification', 'registration_date', 'expiration_date', 'certification_catalog')
+    list_filter = ['certification', 'registration_date', 'expiration_date', 'certification_catalog']
--- packhouses/certifications/models.py
+++ packhouses/certifications/models.py
@@ -1,3 +1,49 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from common.base.models import ProductKind
+from packhouses.catalogs.models import Product
+
+class CertificationCatalog(models.Model):
+    certifier = models.CharField(max_length=255)
+    certification = models.CharField(max_length=255)
+    is_enabled = models.BooleanField(default=True, verbose_name=_('Is enabled'))
+
+    def _str_(self):
+        return f"{self.certifier} -- {self.certification} -- {self.is_enabled}"
+
+class CertificationDocuments(models.Model):
+    name = models.CharField(max_length=255)
+    route = models.FileField(upload_to='documentos/', verbose_name=_('Document'))
+    is_enabled = models.BooleanField(default=True, verbose_name=_('Is enabled'))
+    certification_catalog = models.ForeignKey(CertificationCatalog, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return f"{self.name} -- {self.is_enabled} -- {self.certification_catalog}"
+
+# # class CertificationReports(models.Model):
+# #     name = models.CharField(max_length=255)
+# #     method = models.CharField(max_length=255)
+# #     is_enabled = models.BooleanField(default=True, verbose_name=_('Is enabled'))
+# #     certification_catalog = models.ForeignKey(CertificationCatalog, on_delete=models.CASCADE)
+
+# #     def _str_(self):
+# #         return f"{self.name} -- {self.method} -- {self.is_enabled} -- {self.certification_catalog}"
+
+class CertificationProducts(models.Model): 
+    is_enabled = models.BooleanField(default=True, verbose_name=_('Is enabled'))
+    product_kind = models.ForeignKey(ProductKind, verbose_name=_('Product Kind'), on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, verbose_name=_('Product'), on_delete=models.PROTECT)
+    certification_catalog = models.ForeignKey(CertificationCatalog, verbose_name=_('Certification Catalog'), on_delete=models.CASCADE)
+
+    def _str_(self):
+        return f"{self.is_enabled} -- {self.product_kind} -- {self.product} -- {self.certification_catalog}"
+
+class Certifications(models.Model):
+    certification = models.FileField(upload_to='documentos/', verbose_name=_('Certification'))
+    registration_date = models.DateField()
+    expiration_date = models.DateField()
+    certification_catalog = models.ForeignKey(CertificationCatalog, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return f"{self.certification} -- {self.registration_date}  -- {self.expiration_date}  -- {self.certification_catalog}"
