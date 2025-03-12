eac363fa419bc2fb7f15d87482b774c1d1ef3e53
Daniela Medina <danielamedina@192.168.1.7>
2025-03-11 18:03:42 -0600

    Creacion: Catalogo de certificaciones,11/03/25

--- packhouses/certifications/admin.py
+++ packhouses/certifications/admin.py
@@ -2,54 +2,25 @@ from django import forms
 from django.contrib import admin
 from packhouses.catalogs.models import Product
 from common.base.models import ProductKind
-from .models import (CertificationCatalog, RequirementsCertification, CertificationProducts, Certifications, CertificationsDocuments)
+from .models import (CertificationCatalog, RequirementsCertification, Certifications)#, CertificationProducts, Certifications CertificationsDocuments)
 
 class RequirementsCertificationInline(admin.TabularInline):
     model = RequirementsCertification
-    extra = 1
-
-# -------------------------
-
-class CertificationProductsForm(forms.ModelForm):
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-
-        if db_field.name == "product":
-            if request.POST:
-                product_id = request.POST.get('product_kind') if request.POST else None
-
-
-            if product_kind_id:
-                kwargs["queryset"] = Product.objects.filter(product_kind_id=product_kind_id, is_enabled=True)
-            else:
-                kwargs["queryset"] = Product.objects.none()
-
-            formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
-            formfield.label_from_instance = lambda item: item.name
-            return formfield
-
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-    class Media:
-        js = ('js/admin/forms/packhouses/certifications/products_kind.js',)
-
-
-class CertificationProductsInline(admin.TabularInline):
-    model = CertificationProducts
-    extra = 1
-    form = CertificationProductsForm 
+    extra = 0
 
 @admin.register(CertificationCatalog)
 class CertificationCatalogAdmin(admin.ModelAdmin):
     list_display = ('certifier', 'certification', 'is_enabled')
     list_filter = ['certifier', 'certification', 'is_enabled']
-    inlines = [RequirementsCertificationInline, CertificationProductsInline]
+    inlines = [RequirementsCertificationInline]
+
 
 # class CertificationsDocumentsInline(admin.TabularInline):
 #     model = CertificationsDocuments
 #     extra = 1
 
-# @admin.register(Certifications)
-# class CertificationsAdmin(admin.ModelAdmin):
-#     list_display = ('certification', 'registration_date', 'expiration_date', 'certification_catalog')
-#     list_filter = ['certification', 'registration_date', 'expiration_date', 'certification_catalog']
+@admin.register(Certifications)
+class CertificationsAdmin(admin.ModelAdmin):
+    list_display = ('certification_catalog',)
+#     list_filter = ['certification_catalog']
     # inlines = [CertificationsDocumentsInline]
--- packhouses/certifications/models.py
+++ packhouses/certifications/models.py
@@ -1,23 +1,36 @@
 from django.db import models
 import os
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from common.base.models import ProductKind
 from packhouses.catalogs.models import Product, Organization
-from common.mixins import (CleanNameOrAliasAndOrganizationMixin)
+from common.mixins import (CleanNameOrAliasAndOrganizationMixin, CleanNameAndProductMixin)
 from django.core.validators import FileExtensionValidator
 
 class CertificationCatalog(models.Model):
     certifier = models.CharField(max_length=255)
     certification = models.CharField(max_length=255)
+    product_kind = models.ForeignKey(ProductKind, verbose_name=_('Product Kind'), null=True, blank=True, on_delete=models.PROTECT)
     is_enabled = models.BooleanField(default=True, verbose_name=_('Is enabled'))
 
     def __str__(self):
-        return f"{self.certifier} -- {self.certification} -- {self.is_enabled}"
+        return f"{self.certifier} -- {self.product_kind} -- {self.certification}"
+
+def certification_file_path(instance, filename):
+
+    catalog_id = instance.certification_catalog.id
+    catalog_certifier = slugify(instance.certification_catalog.certifier.replace(" ", ""))
+    catalog_product_kind = slugify(instance.certification_catalog.product_kind).replace(" ", "")
+
+    file_extension = os.path.splitext(filename)[1]
+    file_name = slugify(instance.name.replace(" ", ""))
+
+    return f'certifications/requirements/{catalog_id}_{catalog_product_kind}_{catalog_certifier}_{file_name}{file_extension}'
 
 class RequirementsCertification(models.Model):
     name = models.CharField(max_length=255)
     route = models.FileField(
-        upload_to='certifications/requirements/', 
+        upload_to=certification_file_path, 
         validators=[FileExtensionValidator(allowed_extensions=['docx'])],
         verbose_name=_('Document')
         )
@@ -25,10 +38,53 @@ class RequirementsCertification(models.Model):
     certification_catalog = models.ForeignKey(CertificationCatalog, on_delete=models.CASCADE)
 
     def __str__(self):
-        # print(f"DEBUG: route.name = {self.route.name if self.route else 'No file'}")  # <-- Línea de depuración
-        file_name = os.path.basename(self.route.name) if self.route else "No file"
-        return f"{self.name} -- {file_name} -- {self.is_enabled} -- {self.certification_catalog}"
+        return f"{self.name} -- {self.is_enabled} -- {self.certification_catalog}"
 
+    def save(self, *args, **kwargs):
+        try:
+            old_instance = RequirementsCertification.objects.get(pk=self.pk)
+            if old_instance.route and old_instance.route != self.route:
+                if os.path.isfile(old_instance.route.path):
+                    os.remove(old_instance.route.path)
+        except RequirementsCertification.DoesNotExist:
+            pass 
+
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.route and os.path.isfile(self.route.path):
+            os.remove(self.route.path)
+        super().delete(*args, **kwargs)
+
+class Certifications(models.Model):
+    organization = models.ForeignKey(Organization, verbose_name=_('Organization'), on_delete=models.PROTECT)
+    certification_catalog = models.ForeignKey(CertificationCatalog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.certification_catalog}"
+
+#     class Meta:
+#         verbose_name = _('Certifications')
+#         verbose_name_plural = _('Certifications')
+#         ordering = ('organization', 'certification_catalog')
+#         constraints = [
+#             models.UniqueConstraint(fields=['certification_catalog', 'organization'], name='CertificationProducts_unique_name_organization'),
+#         ]
+
+# class CertificationsDocuments(models.Model):
+#     certification = models.FileField(
+#         upload_to='certifications/certifications/', 
+#         validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
+#         verbose_name=_('Certification')
+#         )
+#     registration_date = models.DateField()
+#     expiration_date = models.DateField()
+#     certification = models.ForeignKey(Certifications, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return f"{self.certification} -- {self.registration_date}  -- {self.expiration_date}  -- {self.certification}"
+
+# ---------------
 # # class ReportsCertification(models.Model):
 # #     name = models.CharField(max_length=255)
 # #     method = models.CharField(max_length=255)
@@ -37,40 +93,3 @@ class RequirementsCertification(models.Model):
 
 # #     def _str_(self):
 # #         return f"{self.name} -- {self.method} -- {self.is_enabled} -- {self.certification_catalog}"
-
-class CertificationProducts(models.Model): 
-    is_enabled = models.BooleanField(default=True, verbose_name=_('Is enabled'))
-    product_kind = models.ForeignKey(ProductKind, verbose_name=_('Product Kind'), null=True, blank=True, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, verbose_name=_('Product'), on_delete=models.PROTECT)
-    certification_catalog = models.ForeignKey(CertificationCatalog, verbose_name=_('Certification Catalog'), on_delete=models.CASCADE)
-
-    def _str_(self):
-        return f"{self.is_enabled} -- {self.product_kind} -- {self.product} -- {self.certification_catalog}"
-
-class Certifications(CleanNameOrAliasAndOrganizationMixin, models.Model):
-    organization = models.ForeignKey(Organization, verbose_name=_('Organization'), on_delete=models.PROTECT)
-    certification_catalog = models.ForeignKey(CertificationCatalog, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.company}  -- {self.certification_catalog}"
-
-    class Meta:
-        verbose_name = _('CertificationProducts')
-        verbose_name_plural = _('CertificationProducts')
-        ordering = ('organization', 'certification_catalog')
-        constraints = [
-            models.UniqueConstraint(fields=['certification_catalog', 'organization'], name='CertificationProducts_unique_name_organization'),
-        ]
-
-class CertificationsDocuments(models.Model):
-    certification = models.FileField(
-        upload_to='certifications/certifications/', 
-        validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
-        verbose_name=_('Certification')
-        )
-    registration_date = models.DateField()
-    expiration_date = models.DateField()
-    certification = models.ForeignKey(Certifications, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.certification} -- {self.registration_date}  -- {self.expiration_date}  -- {self.certification}"
