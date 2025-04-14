5daaac696873607f09c01624f6e5118b04002058
Cesar Benjamin <mathereall@gmail.com>
2025-04-14 15:04:46 -0600

    feat(packing) add packing module with label generation and management features

--- application/settings/base.py
+++ application/settings/base.py
@@ -96,8 +96,6 @@ INSTALLED_APPS = [
     "packhouses.purchases",
     "packhouses.storehouse",
     "packhouses.certifications",
-    "packhouses.receiving",
-    "packhouses.product_packaging",
 
     "eudr.parcels",
 
@@ -376,6 +374,7 @@ JAZZMIN_SETTINGS = {
         "sales",
         "gathering",
         "receiving",
+        "packing",
         "purchases",
         "storehouse",
         "catalogs",
