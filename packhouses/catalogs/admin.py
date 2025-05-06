8c217eae36e6a9fa874086844ff31cf51c1f3d13
cesarbenjamin <mathereall@gmail.com>
2025-05-05 18:10:36 -0600

    fix(PackagingComplementarySupplyInline) se removieron algunas categorias por el issue #164

--- packhouses/catalogs/admin.py
+++ packhouses/catalogs/admin.py
@@ -1446,7 +1446,9 @@ class PackagingComplementarySupplyInline(admin.TabularInline):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         parent_obj_id = request.resolver_match.kwargs.get("object_id")
         parent_obj = Packaging.objects.get(id=parent_obj_id) if parent_obj_id else None
-        packaging_complement_categories = ['packaging_complement', 'packaging_separator', 'packaging_labeling', 'packaging_storage']
+        # packaging_complement_categories = ['packaging_complement', 'packaging_separator', 'packaging_labeling', 'packaging_storage']
+        # se removieron algunas categorias por el issue #164
+        packaging_complement_categories = ['packaging_complement', 'packaging_storage']
 
         if db_field.name == "kind":
             kwargs["queryset"] = SupplyKind.objects.filter(category__in=packaging_complement_categories, is_enabled=True)
