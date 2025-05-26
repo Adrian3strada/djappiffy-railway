d599dfa2d69240711839a22727ee03235e38ca1c
cesarbenjamin <mathereall@gmail.com>
2025-05-26 15:42:33 -0700

    refactor: reorganize models and add Packing model with related fields

--- packhouses/packing/models.py
+++ packhouses/packing/models.py
@@ -1,9 +1,16 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from organizations.models import Organization
+
+from ..catalogs.models import Market, ProductSize, Packaging, ProductPackaging, ProductMarketClass, ProductRipeness, \
+    ProductPhenologyKind
 from ..hrm.models import Employee
+from ..receiving.models import Batch
 from django.utils.translation import gettext_lazy as _
 import uuid
 
 # Create your models here.
+
 
 class PackerLabel(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
@@ -50,10 +57,37 @@ class Package(models.Model):
     packer = models.ForeignKey(PackerEmployee, verbose_name=_('Packer'), on_delete=models.PROTECT)
     scanned_at = models.DateTimeField(null=True, blank=True)
 
+    def __str__(self):
+        scanned_status = "Scanned" if self.scanned_at else "Not Scanned"
+        return f"{self.packer.id}-{self.uuid} | {scanned_status}"
+
     class Meta:
         verbose_name = _('Package')
         verbose_name_plural = _('Packages')
 
+
+class Packing(models.Model):
+    batch = models.ForeignKey(Batch, verbose_name=_('Batch'), on_delete=models.PROTECT)
+    product_size = models.ForeignKey(ProductSize, verbose_name=_('Product size'), on_delete=models.PROTECT)
+    product_phenology = models.ForeignKey(ProductPhenologyKind, verbose_name=_('Product phenology'), on_delete=models.PROTECT, null=True, blank=False)
+    product_market_class = models.ForeignKey(ProductMarketClass, verbose_name=_('Product market class'), on_delete=models.PROTECT, null=True, blank=False)
+    product_ripeness = models.ForeignKey(ProductRipeness, verbose_name=_('Product ripeness'), on_delete=models.PROTECT, null=True, blank=True)
+
+
+    quantity = models.FloatField(verbose_name=_('Quantity'), validators=[MinValueValidator(0.01)])
+
+
+
+
+
+    product_packaging = models.ForeignKey(ProductPackaging, verbose_name=_('Product packaging'), on_delete=models.PROTECT)
+    product_packaging_weight = models.FloatField(_("Product packaging weight"))
+    organization = models.ForeignKey(Organization, verbose_name=_('Organization'), on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        scanned_status = "Scanned" if self.scanned_at else "Not Scanned"
-        return f"{self.packer.id}-{self.uuid} | {scanned_status}"
+        return f"{self.id} - {self.batch} - {self.batch.incomingproduct.scheduleharvest.product.name} - {self.product_size} - {self.batch.incomingproduct.scheduleharvest.market.name} - {self.product_packaging}"
+
+    class Meta:
+        verbose_name = _('Packing')
+        verbose_name_plural = _('Packings')
