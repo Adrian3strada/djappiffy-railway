57e74e3e323959d11a17efffd306d5c34998113e
Niji <mathereall@gmail.com>
2025-06-01 19:12:27 -0600

    refactor: enhance BatchSerializer with market and product fields using SerializerMethodField

--- packhouses/receiving/serializers.py
+++ packhouses/receiving/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from packhouses.receiving.models import Batch
+from packhouses.catalogs.serializers import MarketSerializer, ProductSerializer
 
 
 class BatchSerializer(serializers.ModelSerializer):
+    market = serializers.SerializerMethodField(read_only=True)
+    # product = serializers.SerializerMethodField(read_only=True)
+    product = serializers.SerializerMethodField(source='incomingproduct.scheduleharvest.product', read_only=True)
+    product_phenology = serializers.SerializerMethodField(source='incomingproduct.scheduleharvest.product_phenology', read_only=True)
+
+    def get_market(self, obj):
+        if obj.incomingproduct and obj.incomingproduct.scheduleharvest:
+            return MarketSerializer(obj.incomingproduct.scheduleharvest.market, read_only=True).data
+        return None
+
+    def get_product_aaa(self, obj):
+        if obj.incomingproduct and obj.incomingproduct.scheduleharvest:
+            return ProductSerializer(obj.incomingproduct.scheduleharvest.product, read_only=True).data
+        return None
+
     class Meta:
         model = Batch
         fields = '__all__'
