d163e1ceb7e0e2cee17078a144d4633586f567af
cesarbenjamin <mathereall@gmail.com>
2025-06-02 21:40:47 -0600

    feat: add PackingPallet model and update Batch model's OOID field

--- packhouses/receiving/urls.py
+++ packhouses/receiving/urls.py
@@ -4,7 +4,7 @@ from packhouses.receiving.views import weighing_set_report
 from packhouses.receiving.viewsets import BatchViewSet
 
 
-app_name = 'receiving'  # TODO: preguntar a Jaqueline ¿qué hace esto?
+app_name = 'receiving'
 
 urlpatterns = [
     path('dadmin/receiving/incomingproduct/weighing_set_report/<int:pk>/', weighing_set_report, name='weighing_set_report'),
