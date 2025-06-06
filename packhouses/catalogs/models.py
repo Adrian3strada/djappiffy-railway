a5d699922f6dd044791de35865a86aa2c64695fc
cesarbenjamin <mathereall@gmail.com>
2025-06-06 10:47:34 -0600

    refactor: streamline field definitions in models.py for clarity

--- packhouses/catalogs/models.py
+++ packhouses/catalogs/models.py
@@ -1043,10 +1043,8 @@ class ProductPackaging(CleanNameAndOrganizationMixin, models.Model):
     product = models.ForeignKey(Product, verbose_name=_('Product'), on_delete=models.PROTECT)
     product_size = models.ForeignKey(ProductSize, verbose_name=_('Product size'), on_delete=models.PROTECT)
     packaging = models.ForeignKey(Packaging, verbose_name=_('Packaging'), on_delete=models.CASCADE)
-    product_weight_per_packaging = models.FloatField(verbose_name=_('Product weight per packaging'),
-                                                     validators=[MinValueValidator(0.01)])
-    product_presentation = models.ForeignKey(ProductPresentation, verbose_name=_('Product presentation'),
-                                             null=True, blank=True, on_delete=models.CASCADE)
+    product_weight_per_packaging = models.FloatField(verbose_name=_('Product weight per packaging'), validators=[MinValueValidator(0.01)])
+    product_presentation = models.ForeignKey(ProductPresentation, verbose_name=_('Product presentation'), null=True, blank=True, on_delete=models.CASCADE)
     product_presentations_per_packaging = models.PositiveIntegerField(
         verbose_name=_('Product presentations per packaging'),
         null=True, blank=True, validators=[MinValueValidator(1)])
